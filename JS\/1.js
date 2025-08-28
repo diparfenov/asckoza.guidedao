4ecc20f37486c4cdedf937a7219496cff139c484
diparfenov <asckoza@gmail.com>
2025-08-28 22:11:22 +0200

    12

---
+++ JS\/1.js
@@ -0,0 +1,21 @@
+/* function add(a, b) {
+  return a + b;
+}
+
+const result = add(5, 3);
+console.log("Результат:", result); */
+
+let text = "JavaScript";
+
+console.log(
+  text.length * 8,
+  text.length,
+  text.toLowerCase(),
+  "JavaScript".toUpperCase(),
+  text[1],
+  text[0],
+  text[text.length - 2],
+  "GORE-TEX".substring(5),
+  "Helly Hansen".substring(0, 5),
+  "The North Face".substring(4, 9)
+);
