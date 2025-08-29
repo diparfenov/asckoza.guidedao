97f552b6c5056a2def9b1939187f5aa4f8b839e0
diparfenov <asckoza@gmail.com>
2025-08-29 22:13:41 +0200

    29.08

--- JS\/1.js
+++ JS\/1.js
@@ -5,17 +5,59 @@
 const result = add(5, 3);
 console.log("Результат:", result); */
 
-let text = "JavaScript";
+// let text = "JavaScript";
 
-console.log(
-  text.length * 8,
-  text.length,
-  text.toLowerCase(),
-  "JavaScript".toUpperCase(),
-  text[1],
-  text[0],
-  text[text.length - 2],
-  "GORE-TEX".substring(5),
-  "Helly Hansen".substring(0, 5),
-  "The North Face".substring(4, 9)
-);
+// console.log(
+//   text.length * 8,
+//   text.length,
+//   text.toLowerCase(),
+//   "JavaScript".toUpperCase(),
+//   text[1],
+//   text[0],
+//   text[text.length - 2],
+//   "GORE-TEX".substring(5),
+//   "Helly Hansen".substring(0, 5),
+//   "The North Face".substring(4, 9)
+// );
+
+// let name = "Gleb";
+// name += " Kostin"; // то же самое, что и name = name + " Kostin"
+// name = 2;
+// console.log(name);
+
+// let language = "JavaScript";
+// console.log(`Я учу ${language + `${name + 2}`}`);
+
+// function getFullName(first, last) {
+//   return `${first + " " + last}`;
+// }
+// console.log(getFullName(1, "s"));
+
+// function getFullName(first, last) {
+//   return `${first} ${last}`;
+// }
+// console.log(getFullName("w", "e"));
+
+// function spaceOdysseyTagline() {
+//   return `An epic drama
+// of adventure
+// and exploration`;
+// }
+// console.log(spaceOdysseyTagline());
+
+// function renderTableRow(label, value) {
+//   return `<tr>
+//         <td>здесь ${label}</td>
+//         <td>здесь ${value}</td>
+//         </tr>`;
+// // }
+
+// console.log(renderTableRow("sss", "wwww"));
+
+function getCapitalized(word) {
+  return `${word[0].toUpperCase()}${word.substring(1).toLowerCase()}`;
+}
+
+console.log(getCapitalized("jAREK"));
+console.log(getCapitalized("kasdkKOJSAPFJPOkl"));
+console.log(getCapitalized("AKDASDJKAKSD"));
