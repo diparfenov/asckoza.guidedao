474b138af63c8c6ca753f938aad3185e83181a35
diparfenov <asckoza@gmail.com>
2025-09-23 15:06:32 +0200

    -

---
+++ REACT/react.js
@@ -0,0 +1,23 @@
+// console.log("hello word");
+// const numbers1 = [4, 11, -5, 17, -10, 8, 56, 0];
+// const numbers2 = [
+//   {
+//     id: 1,
+//     number: 10,
+//   },
+//   {
+//     id: 2,
+//     number: -6,
+//   },
+//   {
+//     id: 3,
+//     number: 22,
+//   },
+// ];
+
+// const onlyPositive1 = (mass) => mass.filter((mass) => mass > 0);
+// const onlyPositive2 = (mass) =>
+//   mass.map((mass) => mass.number).filter((n) => n > 0);
+
+// console.log(onlyPositive1(numbers1));
+// console.log(onlyPositive2(numbers2));
