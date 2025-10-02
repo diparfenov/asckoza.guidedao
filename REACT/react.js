535d85f7276e94f0f0217555f4764630cfc67761
diparfenov <asckoza@gmail.com>
2025-10-02 17:55:50 +0200

    1

--- REACT/react.js
+++ REACT/react.js
@@ -437,3 +437,24 @@
 //     </>
 //   );
 // }
+// export const Shop = () => {
+//   const items = [
+//     {
+//       name: "Респиратор 3M",
+//       desc: "Степень защиты FFP2, есть клапан выдоха",
+//       image: "https://source.unsplash.com/400x300/?3M,mask"
+//     },
+//     {
+//       name: "Перчатки латексные",
+//       desc: "Одноразовые перчатки повышенной прочности",
+//       image: "https://source.unsplash.com/400x300/?latex,gloves"
+//     }
+//   ];
+//   return (
+//     <div className="shop">
+//       {items.map((el) => (
+//         <Item key={el.name} {...el} />
+//       ))}
+//     </div>
+//   );
+// };
