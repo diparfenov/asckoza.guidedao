64d298e2d1c8ba7f56b8535a53af5770e0bc6dc1
diparfenov <asckoza@gmail.com>
2026-01-19 14:29:32 +0200

    -

--- REACT/my-ap/src/App.js
+++ REACT/my-ap/src/App.js
@@ -1,36 +1,52 @@
-import { useState, useLayoutEffect } from "react";
-import "./App.css";
+import React, { useState, useLayoutEffect } from "react";
+import { createRoot } from "react-dom/client";
 import mapboxgl from "mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
+import "./styles.css";
+
+const stores = {
+  km20: [37.610641, 55.761994],
+  belief: [37.601152, 55.733396],
+  brandshop: [37.616812, 55.767839],
+};
 
 function App() {
-  const [random, setRandom] = useState(Math.random());
-
   mapboxgl.accessToken =
     "pk.eyJ1IjoiYm9vYW5kcmV3IiwiYSI6ImNrd3M2ZGF1YzBhcDEyb21obzUwcDlvNXMifQ.q0NAPJB4RMgRRI8Fi9PWZg";
+  const [marker, setMarker] = useState();
 
   useLayoutEffect(() => {
     const map = new mapboxgl.Map({
       container: "map",
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: [37.61192, 55.76199],
+      style: "mapbox://styles/mapbox/dark-v10",
+      center: [37.610641, 55.761994],
       zoom: 10,
     });
+    const newMarker = new mapboxgl.Marker().setLngLat(stores.belief).addTo(map);
+
+    setMarker(newMarker);
   }, []);
+
+  const handleSelect = (e) => {
+    const value = e.target.value;
+    value && marker.setLngLat(stores[value]);
+  };
 
   return (
     <>
-      <button
-        className="ui-button"
-        id="rerender"
-        onClick={() => setRandom(Math.random())}
-      >
-        Ререндер!
-      </button>
-      <h1>{random}</h1>
-      <div id="map" style={{ width: "100%", height: "500px" }}></div>
+      <div className="map-overlay">
+        <h3>Выберите магазин: </h3>
+        <select onChange={handleSelect}>
+          <option value="km20">KM20</option>
+          <option value="belief">BELIEF</option>
+          <option value="brandshop">BRANDSHOP</option>
+        </select>
+      </div>
+      <div id="map"></div>
     </>
   );
 }
 
-export default App;
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App />);
