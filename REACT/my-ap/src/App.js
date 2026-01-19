0b16f3b1f91d7aab92dadddbd8a072e168ae0a99
diparfenov <asckoza@gmail.com>
2026-01-19 12:11:23 +0200

    -

--- REACT/my-ap/src/App.js
+++ REACT/my-ap/src/App.js
@@ -1,22 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState, useLayoutEffect } from "react";
 import "./App.css";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [random, setRandom] = useState(Math.random());
 
-  useEffect(() => {
-    document.title = "Привет!";
-    setTimeout(() => {
-      document.title = "Я подсяду?";
-    }, 1000);
-  });
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiYm9vYW5kcmV3IiwiYSI6ImNrd3M2ZGF1YzBhcDEyb21obzUwcDlvNXMifQ.q0NAPJB4RMgRRI8Fi9PWZg";
+
+  useLayoutEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [37.61192, 55.76199],
+      zoom: 10,
+    });
+  }, []);
 
   return (
     <>
-      <h2>Обновлений стейта: {counter}</h2>
-      <button onClick={() => setCounter((prev) => prev + 1)}>
-        Обновить стейт
+      <button
+        className="ui-button"
+        id="rerender"
+        onClick={() => setRandom(Math.random())}
+      >
+        Ререндер!
       </button>
+      <h1>{random}</h1>
+      <div id="map" style={{ width: "100%", height: "500px" }}></div>
     </>
   );
 }
